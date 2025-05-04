ff93275befff170a6ccf55547b686c0c8f17e672
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-04 11:58:12 +0900

    change: ElementData
    Sprite에 대한 직접적인 path 대신 ID 저장
    pixelPerUnit 정보를 SpriteMap으로 이관

--- Assets/Scripts/Scene/Layout/Element/ElementData.cs
+++ Assets/Scripts/Scene/Layout/Element/ElementData.cs
@@ -5,11 +5,10 @@ using System;
 public class ElementData
 {
     public string id;
-    public string streamingAssetsSpritePath;
+    public string spriteID;
     public float x;
     public float y;
     public float width;
     public float height;
     public int sortingOrder;
-    public int pixelPerUnit;
 }
