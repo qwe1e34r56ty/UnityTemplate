0eb8fd6da82d82aa5fa526144948c8f3cb8a22c7
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-04 11:05:11 +0900

    change: mainScene -> startScene 관련 Layout 네이밍 변경

--- Assets/Scripts/Scene/Layout/LayoutID.cs
+++
@@ -1,4 +0,0 @@
-public static class LayoutID
-{
-    public const string Main = "Main";
-}
---
+++ Assets/Scripts/Scene/Layout/LayoutID/ElementID/StartElementID.cs
@@ -0,0 +1,6 @@
+public static class StartElementID
+{
+    public static string Background = "Background";
+    public static string Title = "Title";
+    public static string Start = "Start";
+}
