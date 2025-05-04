3dfe469bf14f44e9983b423d5d7e4bece0ba4034
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-04 10:55:08 +0900

    feat: sprite 관련 ID 추가

---
+++ Assets/Scripts/Resource/Sprite/SpriteID/StartLayoutSpriteID.cs
@@ -0,0 +1,9 @@
+using UnityEditorInternal;
+
+public static class StartLayoutSpriteID
+{
+    public static string Background = "StartLayout/Background";
+    public static string Title = "StartLayout/Title";
+    public static string Start = "StartLayout/Start";
+    public static string StartHoverEnter = "StartLayout/StartHoverEnter";
+}
---
+++ Assets/Scripts/Resource/Sprite/SpritePath.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class SpriteData
+{
+    public string id;
+    public string path;
+    public int pixelPerUnit;
+}
