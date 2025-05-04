6f7267d17737dd14f8b8e813699a43cca7fafb47
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-04 12:03:46 +0900

    change : GameContext 일부 기능 삭제
    컨테이너의 의미에 집중

--- Assets/Scripts/Context/GameContext.cs
+++ Assets/Scripts/Context/GameContext.cs
@@ -1,28 +1,55 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 public class GameContext
 {
-    private SaveData? saveData;
+    public SaveData? saveData = new();
     public Queue<ISceneCommand> SceneCommandQueue = new();
     public Dictionary<string, Dictionary<string, GameObject>> layouts = new();
+    public AScene currentScene = null;
+
+    public Dictionary<string, Sprite> spriteMap = new();
+
+    public Dictionary<string, GameObject> layoutRootMap = new();
     public Dictionary<string, string> layoutPathMap = new();
     public Dictionary<string, LayoutData> layoutDataMap = new();
+
+    public Dictionary<string, string[]> sceneLayoutBindingMap = new();
+    public Dictionary<string, AScene> sceneMap = new();
+
+    public Dictionary<GameObject, Action> onHoverEnterHandlers = new();
+    public Dictionary<GameObject, Action> onHoverExitHandlers = new();
+    public Dictionary<GameObject, Action> onLeftClickHandlers = new();
+
+    public Queue<GameObject> pendingHoverEnterEventQueue = new();
+    public Queue<GameObject> pendingHoverExitEventQueue = new();
+    public Queue<GameObject> pendingLeftClickEventQueue = new();
+
+    public Dictionary<GameObject, Dictionary<KeyCode, Action>> onKeyDownHandlers = new();
+    public Dictionary<GameObject, Dictionary<KeyCode, Action>> onKeyHoldHandlers = new();
+    public Dictionary<GameObject, Dictionary<KeyCode, Action>> onKeyUpHandlers = new();
+
+    public Queue<(GameObject, KeyCode)> pendingKeyDownEventQueue = new();
+    public Queue<(GameObject, KeyCode)> pendingKeyHoldEventQueue = new();
+    public Queue<(GameObject, KeyCode)> pendingKeyUpEventQueue = new();
+
+    public GameContext()
+    {
+    }
+
     public GameContext(SaveData? saveData)
     {
         this.saveData = saveData;
     }
-    public Queue<ISceneCommand> GetSceneCommandQueue()
-    {
-        return SceneCommandQueue;
-    }
 
-    public void ClearLayout()
+    public void ClearEventQueue()
     {
-        layouts.Clear();
-    }
+        pendingHoverEnterEventQueue.Clear();
+        pendingHoverExitEventQueue.Clear();
+        pendingLeftClickEventQueue.Clear();
 
-    public void RegisterLayout(string layoutID, Dictionary<string, GameObject> layout)
-    {
-        layouts.Add(layoutID, layout);
+        pendingKeyDownEventQueue.Clear();
+        pendingKeyHoldEventQueue.Clear();
+        pendingKeyUpEventQueue.Clear();
     }
 }
