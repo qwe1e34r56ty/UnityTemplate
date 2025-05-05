61189a0502ece9daa25849bce8c83f7fb6d6f6bb
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-05 13:42:48 +0900

    feat: JsonPath 내 Layer 관련 path 추가

--- Assets/Scripts/Resource/Json/JsonPath.cs
+++ Assets/Scripts/Resource/Json/JsonPath.cs
@@ -2,4 +2,5 @@ public static class JsonPath{
     public const string LayoutPath = "Layouts/LayoutPaths.json";
     public const string SceneLayoutBinding = "Binding/SceneLayoutBindings.json";
     public const string SpriteData = "Sprites/SpriteDatas.json";
+    public const string LayerID = "Layer/LayerIDs.json";
 }
