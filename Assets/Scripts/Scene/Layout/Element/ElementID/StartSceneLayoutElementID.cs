d3b2e4e0759aaaace4fa5848b769be363a221426
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-06 01:40:40 +0900

    change: Element ID 일부 변경

--- Assets/Scripts/Scene/Layout/Element/ElementID/StartSceneLayoutElementID.cs
+++ Assets/Scripts/Scene/Layout/Element/ElementID/StartSceneLayoutElementID.cs
@@ -3,4 +3,5 @@ public static class StartSceneLayoutElementID
     public static string Background = "Background";
     public static string Title = "Title";
     public static string Start = "Start";
+    public static string Normalize = "Normalization";
 }
