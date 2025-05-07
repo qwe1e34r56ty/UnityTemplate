81651fde3bae69fd8339cc80a68baca8b1a0de79
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-07 12:30:33 +0900

    feat: MainScene 내 오브젝트 생성 관련 코드 추가

--- Assets/Scripts/Scene/MainScene.cs
+++ Assets/Scripts/Scene/MainScene.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class MainScene : AScene
 {
+    GameObject player;
+    GameObject map;
     public MainScene(string SceneID) : base(SceneID)
     {
     }
     public override void Build(GameContext gameContext)
     {
         base.Build(gameContext);
+        player = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Player"));
+        map = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Map"));
     }
     public override void Update(float deltaTime)
     {
@@ -16,5 +20,7 @@ public class MainScene : AScene
     public override void Destroy(GameContext gameContext)
     {
         base.Destroy(gameContext);
+        GameObject.Destroy(player);
+        GameObject.Destroy(map);
     }
 }
