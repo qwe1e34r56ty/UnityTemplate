b96dbf3f7122eda14c6d0e1a094f5b4937a79f52
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-01 18:34:52 +0900

    chore: remove unused ActionID entries

--- Assets/Scripts/Constant/ID/FixedID/ActionID/ActionID.cs
+++ Assets/Scripts/Constant/ID/FixedID/ActionID/ActionID.cs
@@ -1,4 +1,8 @@
-public static class ActionID
+﻿public static class ActionID
 {
-    public static string Reaction = "Reaction";
+    public static string Updateable = "Updateable";
+    public static string Hover = "Hover";
+    public static string HavePolygonCollider = "HavePolygonCollider";
+    public static string GoMainButton = "GoMainButton";
+    public static string GoTitleButton = "GoTitleButton";
 }
