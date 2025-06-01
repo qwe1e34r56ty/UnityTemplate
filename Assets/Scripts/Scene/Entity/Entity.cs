72ac37eddc4416b48ca8686856c5d3421e5c41f5
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-01 18:02:04 +0900

    feat: Entity 수정

--- Assets/Scripts/Scene/Entity/Entity.cs
+++ Assets/Scripts/Scene/Entity/Entity.cs
@@ -1,80 +1,169 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using static UnityEngine.EventSystems.EventTrigger;
 
-public class Entity : EntityBase, IUpdatable
+public class Entity : EntityBase, IUpdateable
 {
     public GameObject root { get; private set; }
-    private Dictionary<string, GameObject> components;
-    private Dictionary<GameObject, AnimationPlayer> animationPlayers;
+    private AnimationPlayer animationPlayer;
+    private HashSet<GameObject> childs;
     private SortedList<int, List<IAction>> sortedActionList;
-    public Entity(GameContext gameContext, 
+    public Entity(GameContext gameContext,
         ResourceManager resourceManager,
         EntityData entityData, 
+        Entity? entityRoot = null,
         Vector3? offsetPosition = null, 
         Vector3? offsetRotation = null, 
         Vector3? offsetScale = null, 
         int offsetSortOrder = 0) : base(entityData)
     {
-        root = new(entityData.id);
-        components = new();
-        animationPlayers = new();
+        root = null;
+        animationPlayer = null;
+        childs = new();
         sortedActionList = new();
-
-        Transform rootTransform = root.transform;
-        rootTransform.position += offsetPosition ?? Vector3.zero;
-        rootTransform.rotation *= Quaternion.Euler(offsetRotation ?? Vector3.zero);
-        rootTransform.localScale = Vector3.Scale(rootTransform.localScale, offsetScale ?? Vector3.one);
-
-        foreach (ComponentData componentData in entityData.componentDataArr)
+        if(entityRoot == null)
         {
-            GameObject component = new(componentData.id);
-
-            Transform componentTransform = component.transform;
-            componentTransform.position += componentData.offsetPosition;
-            componentTransform.rotation *= Quaternion.Euler(componentData.offsetRotation);
-            componentTransform.localScale = Vector3.Scale(componentTransform.localScale, componentData.offsetScale);
-            componentTransform.SetParent(root.transform, false);
-
-            if (gameContext.animationDataMap.TryGetValue(componentData.animationID, out (Sprite[], AnimationPath) animationData)) {
-                SpriteRenderer spriteRenderer = component.AddComponent<SpriteRenderer>();
-                spriteRenderer.sortingOrder += componentData.offsetSortingOrder;
-
-                AnimationPlayer animationPlayer = new();
-                animationPlayer.Play(component, animationData);
-                gameContext.updateHandlers.Add(animationPlayer);
-
-                gameContext.animationPlayerMap.Add(component, animationPlayer);
-                animationPlayers.Add(component, animationPlayer);
-            }
-            components.Add(componentData.id, component);
-            gameContext.entities.Add(component, this);
+            entityRoot = this;
         }
-    }
 
-    public void Update(float deltaTime)
-    {
+        root = new(entityData.id);
+        {
+            SpriteRenderer spriteRenderer = root.AddComponent<SpriteRenderer>();
+            root.layer = LayerMask.NameToLayer(entityData.layerName);
+            root.tag = entityData.tagName;
 
+            Transform rootTransform = root.transform;
+
+            rootTransform.position += offsetPosition ?? Vector3.zero;
+            rootTransform.position += entityData.offsetPosition;
+
+            rootTransform.rotation *= Quaternion.Euler(offsetRotation ?? Vector3.zero);
+            rootTransform.rotation = Quaternion.Euler(entityData.offsetRotation);
+
+            rootTransform.localScale = Vector3.Scale(rootTransform.localScale, offsetScale ?? Vector3.one);
+            rootTransform.localScale = Vector3.Scale(rootTransform.localScale, entityData.offsetScale);
+
+            spriteRenderer.sortingOrder += offsetSortOrder + entityData.offsetSortingOrder;
+        }
+
+        if (gameContext.animationDataMap.TryGetValue(entityData.animationID, out (Sprite[], AnimationPath)animationData))
+        {
+            animationPlayer = new AnimationPlayer();
+            animationPlayer.Play(root, animationData);
+            gameContext.updateHandlers.Add(animationPlayer);
+            gameContext.animationPlayerMap.Add(root, animationPlayer);
+        }
+
+        foreach (EntityData _entityData in entityData.entityDataArr)
+        {
+            Entity entity = new Entity(gameContext, resourceManager, _entityData, entityRoot);
+            childs.Add(entity.root);
+        }
+
+        foreach (var entry in entityData.actionWithPriorityArr)
+        {
+            int priority = entry.priority;
+            if (gameContext.actionMap.TryGetValue(entry.id, out var action))
+            {
+                AttachAction(gameContext, action, priority);
+            }
+        }
+
+        gameContext.entities.Add(root, this);
+        gameContext.entityRoots.Add(root, entityRoot);
     }
 
     public void Destroy(GameContext gameContext)
     {
+        gameContext.entityRoots.Remove(root);
         gameContext.entities.Remove(root);
-        foreach (GameObject gameObject in components.Values)
+
+        foreach (var pair in sortedActionList.ToList())
         {
-            if (animationPlayers.ContainsKey(gameObject))
+            foreach (var action in pair.Value.ToList())
             {
-                {
-                    animationPlayers.Remove(gameObject);
-                }
-                if (gameContext.animationPlayerMap.TryGetValue(gameObject, out AnimationPlayer animationPlayer))
-                {
-                    gameContext.updateHandlers.Remove(animationPlayer);
-                }
-                gameContext.animationPlayerMap.Remove(gameObject);
-                gameContext.entities.Remove(gameObject);
+                DetachAction(gameContext, action);
             }
-            GameObject.Destroy(root);
         }
+        sortedActionList.Clear();
+
+        foreach (GameObject gameObject in childs)
+        {
+            if (gameContext.entities.TryGetValue(gameObject, out Entity entity))
+            {
+                entity.Destroy(gameContext);
+            }
+        }
+
+        if (gameContext.animationPlayerMap.TryGetValue(root, out AnimationPlayer animationPlayer))
+        {
+            gameContext.updateHandlers.Remove(animationPlayer);
+            gameContext.animationPlayerMap.Remove(root);
+        }
+
+        GameObject.Destroy(root);
+    }
+
+    public void Update(GameContext gameContext, float deltaTime)
+    {
+        foreach (var pair in sortedActionList)
+        {
+            foreach (var action in pair.Value)
+            {
+                if (action.CanExecute(gameContext, this, deltaTime))
+                {
+                    action.Execute(gameContext, this, deltaTime);
+                }
+            }
+        }
+    }
+
+    public void AttachAction(GameContext gameContext, IAction action, int priority)
+    {
+        if (!sortedActionList.TryGetValue(priority, out var list))
+        {
+            list = new List<IAction>();
+            sortedActionList[priority] = list;
+        }
+        action.Attach(gameContext, this, priority);
+        list.Add(action);
+    }
+
+    public void DetachAction(GameContext gameContext, IAction actionToRemove)
+    {
+        var oldActionList = new List<(int, List<IAction>)>();
+        foreach (var pair in sortedActionList)
+        {
+            oldActionList.Add((pair.Key, pair.Value));
+        }
+
+        foreach (var pair in oldActionList)
+        {
+            foreach (var action in pair.Item2)
+            {
+                action.Detach(gameContext, this);
+            }
+        }
+
+        ClearAction();
+
+        foreach (var pair in oldActionList)
+        {
+            int priority = pair.Item1;
+            foreach (var action in pair.Item2)
+            {
+                if (action == actionToRemove)
+                    continue;
+
+                AttachAction(gameContext, action, priority);
+            }
+        }
+    }
+
+    public void ClearAction()
+    {
+        sortedActionList.Clear();
     }
 }
