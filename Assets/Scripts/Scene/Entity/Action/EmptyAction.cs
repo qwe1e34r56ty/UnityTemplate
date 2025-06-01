a8d7f6b66828bb34a34dd206cb2b5ebff3e58bd7
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-01 18:12:48 +0900

    feat: implement EmptyAction for IAction template

---
+++ Assets/Scripts/Scene/Entity/Action/EmptyAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+public class EmptyAction : IAction
+{
+    public EmptyAction()
+    {
+
+    }
+
+    public void Attach(GameContext gameContext, Entity entity, int priority)
+    {
+        Logger.Log($"[EmptyAction] Attacehd on [{entity.root.name}]");
+    }
+
+    public void Detach(GameContext gameContext, Entity entity)
+    {
+        Logger.Log($"[EmptyAction] Detached from [{entity.root.name}]");
+    }
+
+    public bool CanExecute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+        return false;
+    }
+
+    public void Execute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+    }
+}
