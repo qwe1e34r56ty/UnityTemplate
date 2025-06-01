78a799d4565d1bea148f88cd2ad0ea4da8614b2e
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-01 18:12:31 +0900

    feat: implement GoMainButtonAction for scene move

---
+++ Assets/Scripts/Scene/Entity/Action/GoMainButtonAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+public class GoMainButtonAction : IAction
+{
+    public GoMainButtonAction()
+    {
+
+    }
+
+    public void Attach(GameContext gameContext, Entity entity, int priority)
+    {
+        gameContext.onLeftClickHandlers.Add(entity.root, () =>
+        {
+            gameContext.sceneCommandQueue.Enqueue(new ConvertSceneCommand(SceneID.Main, $"[{entity.root.name}] request convert to [{SceneID.Main}] scene"));
+        });
+    }
+
+    public void Detach(GameContext gameContext, Entity entity)
+    {
+        gameContext.onLeftClickHandlers.Remove(entity.root);
+    }
+
+    public bool CanExecute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+        return false;
+    }
+
+    public void Execute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+    }
+}
