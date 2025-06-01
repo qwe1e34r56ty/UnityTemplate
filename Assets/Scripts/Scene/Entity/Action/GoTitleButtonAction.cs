96109a2b88c311668cc3902286c88fa91a1f9187
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-01 18:12:21 +0900

    feat: implement GoTitleButtonAction for scene move

---
+++ Assets/Scripts/Scene/Entity/Action/GoTitleButtonAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+public class GoTitleButtonAction : IAction
+{
+    public GoTitleButtonAction()
+    {
+
+    }
+
+    public void Attach(GameContext gameContext, Entity entity, int priority)
+    {
+        gameContext.onLeftClickHandlers.Add(entity.root, () =>
+        {
+            gameContext.sceneCommandQueue.Enqueue(new ConvertSceneCommand(SceneID.Start, $"[{entity.root.name}] request convert to [{SceneID.Main}] scene"));
+        });
+    }
+
+    public void Detach(GameContext gameContext, Entity entity)
+    {
+        gameContext.onLeftClickHandlers.Remove(entity.root);
+    }
+
+    public bool CanExecute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+        return false;
+    }
+
+    public void Execute(GameContext gameContext, Entity entity, float deltaTIme)
+    {
+    }
+}
