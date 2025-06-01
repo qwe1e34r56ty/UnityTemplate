003491de01bd72a6cd51975c934744e67e26f5c0
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-01 23:09:43 +0900

    feat: implement stat parse strategy for int stat

---
+++ Assets/Scripts/Scene/Entity/StatParseStrategy/IntParseStrategy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+#nullable enable
+
+public class IntParseStrategy : IStatParseStrategy<int>
+{
+    public bool TryGetStat(Dictionary<string, string> stats, string key, out int ret)
+    {
+        ret = default;
+        if (stats.TryGetValue(key, out var str))
+        {
+            return int.TryParse(str, out ret);
+        }
+        return false;
+    }
+
+    public int GetStat(Dictionary<string, string> stats, string key)
+    {
+        if (TryGetStat(stats, key, out var result))
+        {
+            return result;
+        }
+        return default;
+    }
+
+    public int SetStat(Dictionary<string, string> stats, string key, int value)
+    {
+        stats[key] = value.ToString();
+        return value;
+    }
+}
