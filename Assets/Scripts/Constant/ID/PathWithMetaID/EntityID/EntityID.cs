d66443d9f650dba975b7f9410fde591d966252e7
qwe1e34r56ty <sunwoonana@gmail.com>
2025-06-11 17:39:36 +0900

    refactor: move directory

--- Assets/Scripts/Constant/ID/PathWithMetaID/EntityID/EntityID.cs
+++
@@ -1,6 +0,0 @@
-public static class EntityID
-{
-    public const string StartScene = "StartScene";
-    public const string GoMainButton = "GoMainButton";
-    public const string MainScene = "MainScene";
-}
